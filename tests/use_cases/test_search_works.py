f11a57bd44acdda212e7ff190fbe2c3e003c1835
slash0t <minenion2@gmail.com>
2025-11-16 23:55:57 +0300

    Test search works

--- tests/use_cases/test_search_works.py
+++ tests/use_cases/test_search_works.py
@@ -1,38 +1,30 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 
 URL_SEARCH = "https://moe-online.ru/search"
-SEARCH_QUERY = "погода"   # слово, которое точно есть в новостях
+SEARCH_QUERY = "погода"
 
 
 def test_search_returns_non_empty_result_list(driver):
     driver.get(URL_SEARCH)
     wait = WebDriverWait(driver, 15)
 
-    # Поле поиска
     SEARCH_INPUT = (
         By.CSS_SELECTOR,
-        "input[type='search']"      # при необходимости замени на реальный селектор
-        # например: "input[name='text']" или "input.search__input"
+        "input.search_text"
     )
 
-    # Кнопка "Найти"
     SEARCH_BUTTON = (
         By.CSS_SELECTOR,
-        "button[type='submit']"     # тоже можно уточнить, если у кнопки есть свой класс
-        # например: "form[action*='/search'] button"
+        "button.btn-default"
     )
 
-    # Элементы результатов поиска (карточки новостей)
     SEARCH_RESULT_ITEMS = (
         By.CSS_SELECTOR,
-        "a[href*='/news/']"         # общее, но рабочее правило: ссылки на новости
-        # можно заменить на более точный селектор, если найдетcя, например: "div.search-result-item"
+        "div.archive-list div.material-row-list:nth-child(1)"
     )
 
-    # 1. Ждём поле поиска и кнопку
     search_input = wait.until(
         EC.visibility_of_element_located(SEARCH_INPUT)
     )
@@ -40,28 +32,14 @@ def test_search_returns_non_empty_result_list(driver):
         EC.element_to_be_clickable(SEARCH_BUTTON)
     )
 
-    # 2. Вводим запрос
     search_input.clear()
     search_input.send_keys(SEARCH_QUERY)
 
-    # 3. Нажимаем "Найти"
     search_button.click()
 
-    # 4. Ждём появления результатов (минимум один элемент)
-    results = wait.until(
-        lambda d: d.find_elements(*SEARCH_RESULT_ITEMS)
-    )
+    def search_result_list(driver_):
+        return driver_.find_element(*SEARCH_RESULT_ITEMS)
 
-    # Проверяем, что список не пустой
-    assert len(results) > 0, "Список результатов поиска оказался пустым при корректном запросе"
+    result = wait.until(search_result_list )
 
-    # 5. Дополнительно убеждаемся, что нет сообщения «Ничего не найдено»
-    nothing_found_xpath = "//*[contains(text(),'Ничего не найдено')]"
-
-    try:
-        driver.find_element(By.XPATH, nothing_found_xpath)
-        nothing_found_visible = True
-    except NoSuchElementException:
-        nothing_found_visible = False
-
-    assert not nothing_found_visible, "Отобразилось сообщение 'Ничего не найдено' при корректном запросе"
+    assert result is not None
